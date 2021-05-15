cf15c740d82518b9ef7809ee573f75f5565e6c87
Lucas <lucas_backend@kakao.com>
2021-05-15 11:58:32 +0900

    Add service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,45 +8,49 @@ import {
   Post,
   Put,
   Query,
-} from '@nestjs/common';
+} from '@nestjs/common'
+import { Movie } from './entities/movie.entity'
+import { MoviesService } from './movies.service'
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'All Movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll()
   }
 
   @Get('search')
   search(@Query('year') year: string) {
-    console.log(typeof year);
+    console.log(typeof year)
 
-    return `We are searching for a movie made after : ${year}`;
+    return `We are searching for a movie made after : ${year}`
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie with the id : ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId)
   }
 
   @Post()
   create(@Body() movieData) {
-    return 'This will create a movie';
+    return this.moviesService.create(movieData)
   }
 
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `This will remove one movie with the id : ${movieId}`;
+    return this.moviesService.deleteOne(movieId)
   }
 
   // 일부분 수정할 때 사용됨
   @Patch(':id')
   patch(@Param('id') movieId: string) {
-    return `This will patch one movie with the id : ${movieId}`;
+    return `This will patch one movie with the id : ${movieId}`
   }
 
   @Put(':id')
   put(@Param('id') movieId: string) {
-    return `This will put one movie with the id : ${movieId}`;
+    return `This will put one movie with the id : ${movieId}`
   }
 }
