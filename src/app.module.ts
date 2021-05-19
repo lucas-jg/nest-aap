4f17825a52d355a7b558d11d26d8bdb1a18ecd34
Lucas <lucas_backend@kakao.com>
2021-05-19 18:57:58 +0900

    Add modules and Dependency injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
-import { ContentsController } from './contents/contents.controller';
-import { ContentsService } from './contents/contents.service';
+import { Module } from '@nestjs/common'
+import { MoviesController } from './movies/movies.controller'
+import { MoviesService } from './movies/movies.service'
+import { ContentsController } from './contents/contents.controller'
+import { ContentsService } from './contents/contents.service'
+import { MoviesModule } from './movies/movies.module'
 
 @Module({
-  imports: [],
-  controllers: [MoviesController, ContentsController],
-  providers: [MoviesService, ContentsService],
+  imports: [MoviesModule],
+  controllers: [ContentsController],
+  providers: [ContentsService],
 })
 export class AppModule {}
